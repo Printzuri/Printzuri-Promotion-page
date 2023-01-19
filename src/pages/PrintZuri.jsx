91f95c91da02cb6450d822b4b700e9ff7ba78d2e
Chisquare7 <chijiokechibuike.f@gmail.com>
2023-01-20 00:33:59 +0100

    first commit of printzuri promotional page

---
+++ src/pages/PrintZuri.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const PrintZuri = () => {
+  return (
+		<div className="overall__container">
+			{/* <div>
+				<img
+					src="https://res.cloudinary.com/dhupahyac/image/upload/v1674167865/Coming_Soon_1_gpk2tu.webp"
+					alt=""
+				/>
+			</div> */}
+			<div className="main__container">
+				<nav className="nav__content">
+					<h1>We are building something amazing! </h1>
+				</nav>
+				<div className="page__content"></div>
+			</div>
+		</div>
+	);
+}
+
+export default PrintZuri
