2443b902952072fc71f5d5552bbad9d436dae9e3
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2023-03-12 19:34:13 +0100

    feat: added the achor link and icon

--- src/pages/PrintZuri.jsx
+++ src/pages/PrintZuri.jsx
@@ -8,10 +8,12 @@ const PrintZuri = () => {
     <div className="commingSoon">
       <div className="commingSoonCont">
         <nav className="commingSoonNav">
+        <a href="https://printzuri.netlify.app">
           <img
             src="https://res.cloudinary.com/dhupahyac/image/upload/v1674152549/printzuri-logo_a1qexe.png"
             alt="Brand Logo"
           />
+          </a>
         </nav>
         <section className="commingSoonSec">
           <h1>We are building something amazing!</h1>
@@ -27,8 +29,12 @@ const PrintZuri = () => {
 			<button type="submit">Notify Me</button>
 		  </form>
 		  <div className="commingSoonSocials">
-			<img src={socialInsta} alt="social insta" />
-			<img src={socialTweet} alt="social twitter" />
+      <a href="https://www.instagram.com/printzuri/" target="_blank" rel="noreferrer">
+			  <img src={socialInsta} alt="social insta" />
+      </a>
+      <a href="https://twitter.com/printzuri" target="_blank" rel="noreferrer">
+			  <img src={socialTweet} alt="social twitter" />
+      </a>
 		  </div>
         </section>
       </div>
