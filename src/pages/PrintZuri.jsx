9f539ece06af567886133e135b016ada4c63b4ce
Chisquare7 <chijiokechibuike.f@gmail.com>
2023-06-27 15:17:50 +0100

    Feat: set up the hero and first section markup and styling of the hero section

--- src/pages/PrintZuri.jsx
+++ src/pages/PrintZuri.jsx
@@ -1,12 +1,54 @@
 import React from "react";
 import "./styles/PrintZuri.css";
+import heroIcon from "../assets/png/hero__img.png"
+import uniqueIcon from "../assets/png/unique__img.png"
 
 const PrintZuri = () => {
-  return (
-    <>
-      <h1>Coming Soon</h1>
-    </>
-  );
+	return (
+		<>
+			<section className="heroSection">
+				<div className="container">
+					<div className="heroText">
+						<div className="heroHeading">
+							<h1 className="heroHead">Everyday products that helps you stand out</h1>
+						</div>
+						<div className="heroSubhead">
+							<p className="heroSubtext">
+								Printzuri is your go-to platform for your quality printed
+								product as a buyer or seller. Choose your desired product and we
+								will cover the rest.
+							</p>
+						</div>
+					</div>
+					<div className="videoContainer">
+						<img src={heroIcon} alt="PrintZuri video icon" />
+					</div>
+				</div>
+			</section>
+			<section className="firstSection">
+				<div className="container">
+					<div className="flexRow">
+						<div className="flexCol">
+							<div className="columnOne">
+								<div className="uniqueHeading">
+									<h2 className="uniqueHead">What makes Printzuri unique?</h2>
+								</div>
+								<div className="uniqueSubhead">
+									<p className="uniqueSubtext">
+										You can create a store with Printzuri and monetize your
+										creations.
+									</p>
+								</div>
+							</div>
+						</div>
+						<div className="flexCol">
+							<img src={uniqueIcon} alt="Unique section icon" />
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default PrintZuri;
