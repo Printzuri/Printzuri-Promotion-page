367b28f43bcf597d4c1707a5b3f6af5383250017
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2023-07-02 08:41:11 +0100

    feat: created the element for the footer

--- src/pages/PrintZuri.jsx
+++ src/pages/PrintZuri.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import "./styles/PrintZuri.css";
+import { ReactComponent as Youtube } from './../assets/svg/YouTube.svg'
+import { ReactComponent as Twitter } from './../assets/svg/Twitter.svg'
+import { ReactComponent as Instagram } from './../assets/svg/Instagram.svg'
+import logoImg from './../assets/png/Logo.png'
+import { Link } from "react-router-dom";
 
 const PrintZuri = () => {
   return (
     <>
-      <h1>Coming Soon</h1>
+      <footer className="promoFooter">
+        <div className="container">
+          <p className="promoFooterDetail">
+          If you would like to find out more about Printzuri, or if you have any questions, please reach out to us via hello@printzuri.com
+          </p>
+          <nav className="promoFooterNav">
+            <p>© 2022 Printzuri</p>
+            <div className="promoFooterNavSocial">
+              <Link to="/"><Youtube /></Link>
+              <Link to="/"><Twitter /></Link>
+              <Link to="/"><Instagram /></Link>
+            </div>
+            <img src={logoImg} alt="logo"/>
+          </nav>
+        </div>
+      </footer>
     </>
   );
 };
