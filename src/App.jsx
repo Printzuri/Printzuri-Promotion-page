159e388b3c4880644d2c90c43be2cf624b007946
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2023-07-10 16:43:06 +0100

    implemented the modal on the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import PrintZuri from "./pages/PrintZuri";
+import ModalScreen from "./components/ModalScreen";
 import "./App.css";
 function App() {
   return (
 		<div className="App">
 			<Routes>
 				<Route path="/" element={<PrintZuri />} />
+				{/* <Route path="/modal" element={<ModalScreen />} /> */}
 			</Routes>
 		</div>
 	);
